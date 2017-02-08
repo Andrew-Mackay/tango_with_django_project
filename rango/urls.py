e9b8d2b5fba1ce4f31275fc0f9f644e322c1caaa
Andrew Mackay <2192793m@student.gla.ac.uk>
2017-02-08 22:22:53 +0000

    Completed up to 9.10

--- rango/urls.py
+++ rango/urls.py
@@ -17,4 +17,6 @@ urlpatterns = [
 		name='register'),
 		
 	url(r'^login/$', views.user_login, name='login'),
+	url(r'^restricted/', views.restricted, name='restricted'),
+	url(r'^logout/', views.user_logout, name='logout'),
 ]
